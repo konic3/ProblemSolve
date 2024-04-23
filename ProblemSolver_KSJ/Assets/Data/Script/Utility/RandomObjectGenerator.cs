6c315cd24460fa0f32fe7aa4eb607e2c7e10f4a1
konic3 <aa22856005@gmail.com>
2024-04-23 12:46:21 +0900

    generate circle

--- ProblemSolver_KSJ/Assets/Data/Script/Utility/RandomObjectGenerator.cs
+++ ProblemSolver_KSJ/Assets/Data/Script/Utility/RandomObjectGenerator.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 
 public class RandomObjectGenerator : MonoBehaviour
 {
     public GameObject TargetObject;
+    public float Radius = 10.0f; // 원의 반지름 설정
     public int ObjectNumber = 0;
 
 #if UNITY_EDITOR
@@ -33,29 +30,18 @@ public class RandomObjectGenerator : MonoBehaviour
         for (int i = 0; i < ObjectNumber; i++)
         {
             // 랜덤한 위치를 생성합니다.
-            Vector3 randomPosition = new Vector3(
-                Random.Range(transform.position.x - transform.localScale.x * 0.5f, transform.position.x + transform.localScale.x * 0.5f),
-                Random.Range(transform.position.y - transform.localScale.y * 0.5f, transform.position.y + transform.localScale.y * 0.5f),
-                Random.Range(transform.position.z - transform.localScale.z * 0.5f, transform.position.z + transform.localScale.z * 0.5f)
-            );
+            Vector2 randomCircle = Random.insideUnitCircle * Radius;
+            Vector3 randomPosition = transform.position + new Vector3(randomCircle.x, 0.0f, randomCircle.y);
 
             // TargetObject를 생성하고 랜덤한 위치에 배치합니다.
             GameObject newObject = Instantiate(TargetObject, randomPosition, Quaternion.identity);
         }
-
     }
 
     private void OnDrawGizmosSelected()
     {
-        // Box 형태의 가이드라인을 그립니다.
+        // 원 형태의 가이드라인을 그립니다.
         Handles.color = Color.yellow;
-
-        Matrix4x4 cubeTransform = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
-
-        using (new Handles.DrawingScope(cubeTransform))
-        {
-            Vector3 from = transform.position + transform.forward * 1.0f;
-            Handles.DrawWireArc(transform.position, transform.up, from, 360.0f, 1.0f);
-        }
+        Handles.DrawWireDisc(transform.position, transform.up, Radius);
     }
 }
